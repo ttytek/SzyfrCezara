5891856d307a1acec0a4bf96d293b161a173052f
tw448537 <ttytek@gmail.com>
2023-05-30 17:15:18 +0200

    File reader initial commit

---
+++ FileReader.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileReader {
+    String readLine(String fileName){
+        File file = new File(fileName);
+        Scanner scanner;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "error";
+        }
+
+        if (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            scanner.close();
+            return line;
+        }
+        scanner.close();
+        return "error";
+    }
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Siema Kacper");
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+
     }
 }
