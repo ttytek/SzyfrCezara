72c79aa1f4702fd62b0600f3248eae75593a4dec
kacper2204 <kacper2204@gmail.com>
2023-05-30 17:45:02 +0200

    cleaned up the main class

--- Main.java
+++ Main.java
@@ -2,9 +2,5 @@ import java.util.EmptyStackException;
 
 public class Main {
     public static void main(String[] args){
-        Encrypter encrypter = new Encrypter(1);
-        String xd = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        System.out.println(encrypter.encrypt(xd));
-        System.out.println(encrypter.decrypt(xd));
     }
 }
