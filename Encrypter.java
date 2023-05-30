a6e09ccee644c0eae0894982f59238d2e9d01b24
kacper2204 <kacper2204@gmail.com>
2023-05-30 17:29:42 +0200

    Perfectly working final version of encrypter by Kacper!

---
+++ Encrypter.java
@@ -0,0 +1,37 @@
+public class Encrypter {
+    private int shift_value;
+    public Encrypter(int shift_value){
+        this.shift_value = shift_value;
+    }
+
+
+    public String encrypt(String message){
+        String encryptedMessage = new String();
+        for(char letter : message.toCharArray()){
+            if((int)letter <= 122 && (int)letter >= 97){    //mala litera
+                letter -= 97;
+                letter += shift_value;
+                letter %= 26;
+                letter += 97;
+            }
+            if((int)letter <= 90 && (int)letter >= 65){     //wielka litera
+                letter -= 65;
+                letter += shift_value;
+                letter %= 26;
+                letter += 65;
+            }
+            encryptedMessage += letter;
+        }
+        return encryptedMessage;
+    }
+
+    public String decrypt(String message){
+        shift_value *= -1;
+        shift_value += 26;
+        String decryptedMessage = encrypt(message);
+        shift_value -= 26;
+        shift_value *= -1;
+        return decryptedMessage;
+    }
+
+}
