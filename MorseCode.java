584ef5ef88e867ae486579fa57cad30f053f76b7
kacper2204 <kacper2204@gmail.com>
2023-06-06 12:09:41 +0200

    Added encode method.

--- MorseCode.java
+++ MorseCode.java
@@ -41,4 +41,25 @@ public class MorseCode {
                     "-.--",     //y
                     "--..",     //z
             };
+    public String encode(String message){
+        String encodedMessage = new String();
+        for(char letter : message.toCharArray()){
+            if((int)letter <=122 && (int) letter >= 97){    //mala litera
+                letter -= 97;
+                encodedMessage += " ";
+                encodedMessage += letters[letter];
+            }
+            if((int)letter <= 90 && (int)letter >= 65){     //wielka litera
+                letter -= 65;
+                encodedMessage += " ";
+                encodedMessage += letters[letter];
+            }
+            if((int)letter <= 57 && (int)letter >= 48){
+                letter -= 48;
+                encodedMessage += " ";
+                encodedMessage += numbers[letter];
+            }
+        }
+        return encodedMessage;
+    }
 }
