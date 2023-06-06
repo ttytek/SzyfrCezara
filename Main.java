bc1eb7de13c57e33904281a73181e9198704bd7f
kacper2204 <kacper2204@gmail.com>
2023-06-06 12:10:07 +0200

    Added encode method.

--- Main.java
+++ Main.java
@@ -2,7 +2,5 @@ import java.util.EmptyStackException;
 
 public class Main {
     public static void main(String[] args){
-        char letter = '0';
-        System.out.println((int)letter);
     }
 }
